0479f8227e14db8e9c76ca32e7a8fe951fd8389d
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-12-07 15:19:43 -0300

    :art: Adicionada as rotas
    corrigida as rotas e a pasta src que estava errada

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import drinkIncon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+
+const linkElement = (imgSrc, linkTestId, url) => (
+  <a
+    href={ url }
+    data-testid={ linkTestId }
+    src={ imgSrc }
+    alt=""
+    style={ { height: '50px', width: '50px' } }
+    className="footer-icon"
+  >
+    <img src={ imgSrc } alt="" />
+  </a>
+);
+
+function Footer() {
+  return (
+    <footer data-testid="footer" className="footer-container">
+      <section className="footer-icons">
+        { linkElement(drinkIncon, 'drinks-bottom-btn', '/bebidas', 'bebidas') }
+        { linkElement(exploreIcon, 'explore-bottom-btn', '/explorar', 'explore') }
+        { linkElement(mealIcon, 'food-bottom-btn', '/comidas', 'comidas') }
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
