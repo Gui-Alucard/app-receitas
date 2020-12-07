07d1a61c9a6c98b907a4d958192b7eca9245ac52
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-12-07 19:52:11 -0300

    :bug: fix routes
    /app-receitas where do not have exact

--- src/App.js
+++ src/App.js
@@ -26,32 +26,41 @@ function App() {
   return (
     <ReceitasProvider>
       <Switch>
-        <Route path="/bebidas/:id/in-progress" component={ ReceitaEmProgresso } />
-        <Route path="/comidas/:id/in-progress" component={ ReceitaEmProgresso } />
-        <Route path="/comidas/:id" component={ ReceitaDetalhada } />
-        <Route path="/bebidas/:id" component={ ReceitaDetalhada } />
-        <Route path="/comidas" component={ Comidas } />
-        <Route path="/bebidas" component={ Bebidas } />
         <Route
-          path="/explorar/comidas/ingredientes"
+          path="/app-receitas/bebidas/:id/in-progress"
+          component={ ReceitaEmProgresso }
+        />
+        <Route
+          path="/app-receitas/comidas/:id/in-progress"
+          component={ ReceitaEmProgresso }
+        />
+        <Route path="/app-receitas/comidas/:id" component={ ReceitaDetalhada } />
+        <Route path="/app-receitas/bebidas/:id" component={ ReceitaDetalhada } />
+        <Route path="/app-receitas/comidas" component={ Comidas } />
+        <Route path="/app-receitas/bebidas" component={ Bebidas } />
+        <Route
+          path="/app-receitas/explorar/comidas/ingredientes"
           component={ ExplorarComidasIngredientes }
         />
         <Route
-          path="/explorar/bebidas/ingredientes"
+          path="/app-receitas/explorar/bebidas/ingredientes"
           component={ ExplorarBebidasIngredientes }
         />
         <Route
-          path="/explorar/comidas/area"
+          path="/app-receitas/explorar/comidas/area"
           component={ ExplorarComidasPorLocalOrigem }
         />
-        <Route path="/explorar/bebidas/area" component={ NotFound } />
-        <Route path="/receitas-feitas" component={ ReceitasFeitas } />
-        <Route path="/receitas-favoritas" component={ ReceitasFavoritas } />
-        <Route path="/perfil" component={ Perfil } />
-        <Route path="/explorar/comidas" component={ ExplorarComidas } />
-        <Route path="/explorar/bebidas" component={ ExplorarBebidas } />
-        <Route path="/explorar" component={ Explorar } />
-        <Route exact path="/app-receitas/" component={ Login } />
+        <Route
+          path="/app-receitas/explorar/bebidas/area"
+          component={ NotFound }
+        />
+        <Route path="/app-receitas/receitas-feitas" component={ ReceitasFeitas } />
+        <Route path="/app-receitas/receitas-favoritas" component={ ReceitasFavoritas } />
+        <Route path="/app-receitas/perfil" component={ Perfil } />
+        <Route path="/app-receitas/explorar/comidas" component={ ExplorarComidas } />
+        <Route path="/app-receitas/explorar/bebidas" component={ ExplorarBebidas } />
+        <Route path="/app-receitas/explorar" component={ Explorar } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </ReceitasProvider>
   );
