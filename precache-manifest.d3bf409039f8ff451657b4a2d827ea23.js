4df7361ea5e8b38b0006cb8ad8969347952a174a
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-12-07 15:15:23 -0300

    Updates

---
+++ precache-manifest.d3bf409039f8ff451657b4a2d827ea23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcbcad8fcc00f10a363753c1310f6e8",
+    "url": "/app-receitas/index.html"
+  },
+  {
+    "revision": "0026abdad82adee47f6c",
+    "url": "/app-receitas/static/css/main.3636a15a.chunk.css"
+  },
+  {
+    "revision": "55908531f47451a236a7",
+    "url": "/app-receitas/static/js/2.1e172ef5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/app-receitas/static/js/2.1e172ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0026abdad82adee47f6c",
+    "url": "/app-receitas/static/js/main.f2fb1510.chunk.js"
+  },
+  {
+    "revision": "f0cd473e09855cc953e0",
+    "url": "/app-receitas/static/js/runtime-main.0f6d492e.js"
+  }
+]);
